0ad300d76454b5ade82bf8129d145f30cf5d38bf
Lexmark444 <lexmark444@gmail.com>
2022-05-24 22:15:47 -0400

    ch7 complete

---
+++ Pre-work/Chapter7 (User input and While Loops)/test.py
@@ -0,0 +1,16 @@
+i = 6
+while i <= 10:
+    for j in range(3):
+        i*=j+2
+        print(j, end = " ")
+        break
+
+
+while False:
+    print("My", end=" ")
+    print("Name", end=" ")
+    print("is", end=" ")
+    continue
+    print("chika-chika", end=" ")
+    break
+    print("slim shady", end=" ") 
